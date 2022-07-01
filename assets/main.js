0da0edeab75f0d0cfd125b263c3d7ba2b44651fb
jonpapayon <whyjonpapayon@gmail.com>
2022-07-01 15:28:01 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=7ghhRHRP6t4&part=id%2Csnippet&type=video&maxResults=5'
+
+const content = null || document.getElementById('content')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b937b03c67msh7501553d3d2392fp1081edjsn3db825656394',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi)
+    const data = await response.json
+    return data
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API)
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+        `).slice(0,4).join('')}
+        `
+        content.innerHTML = view
+    } catch (error) {
+        console.log(error)
+    }
+})()
